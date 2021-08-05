de4dd1fba565ec4ff948bf9c1596456e32fdc39f
Samuel <s.okoro07@gmail.com>
2021-08-05 09:02:55 +0100

    Accessibility record and coupe possessions

---
+++ canvas/setup.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+var roadside;
+var accessibility;
+var coupe;
+var possessions;
+var record;
+
+const setup = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default setup
